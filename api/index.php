c983dc644547055c3ad984ef16c4dc02b758fcbc
Yafaooo <yafaomabelomberz@gmail.com>
2026-03-19 01:23:49 +0700

    Final: restoring api/index.php and cleaning up for fresh Vercel re-import

---
+++ api/index.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Vercel Entry Point for Laravel
+ */
+
+require __DIR__ . '/../public/index.php';
--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<h1>EduSmart AI: PHP Runtime Test Success</h1>";
-echo "<p>If you see this, PHP is running correctly on Vercel.</p>";
-phpinfo();
